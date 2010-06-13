6c41c6e907bc5de2869680395a23e229d39bae29
Hajime Hoshi <hajimehoshi@gmail.com>
2010-06-13 23:51:57 +0900

    added MainScene

--- src/kaiko/game/application.d
+++ src/kaiko/game/application.d
@@ -7,10 +7,28 @@ import kaiko.game.drawablecollection;
 import kaiko.game.sprite;
 import kaiko.game.texture;
 
+final class ExitException : Exception {
+
+  private immutable int status_;
+
+  public this(int status) {
+    super("Exit");
+    this.status_ = status;
+  }
+
+  @property public int status() const {
+    return this.status_;
+  }
+
+}
+
 final class Application {
 
   public static immutable width = 256;
   public static immutable height = 224;
+
+  private this() {
+  }
 
   @property
   public static int cmdShow() {
@@ -34,52 +52,29 @@ final class Application {
     return moduleHandle;
   }
 
-  public static int run(Device)(HWND hWnd, Device device) in {
+  public static int run(Device, Scene)(HWND hWnd, Device device, Scene scene) in {
     assert(hWnd);
   } body {
     ShowWindow(hWnd, typeof(this).cmdShow);
     if (!UpdateWindow(hWnd)) {
       throw new Exception(sysErrorString(GetLastError()));
     }
-    MSG msg;
-    auto texture = new Texture(device, "d.png");
-    auto sprites = new Sprite!(typeof(texture))[2];
-    for (int i = 0; i < sprites.length; i++) {
-      sprites[i] = new Sprite!(typeof(texture))(texture);
-    }
-    auto drawableCollection = new DrawableCollection!(typeof(sprites[0]))(sprites);
+    scene.run(device);
+    return 0;
+  }
 
-    while (msg.message != WM_QUIT) {
-      if (PeekMessage(&msg, null, 0, 0, PM_REMOVE)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-      } else {
-        Sleep(1);
-        foreach (i, sprite; sprites) {
-          sprite.affineMatrix.tx = i * 50;
-          sprite.affineMatrix.ty = i * 50;
-          sprite.z = i;
-          auto colorMatrix = sprite.colorMatrix;
-          if (i == 1) {
-            foreach (j; 0..3) {
-              colorMatrix[j, 0] = 0.2989;
-              colorMatrix[j, 1] = 0.5866;
-              colorMatrix[j, 2] = 0.1145;
-              colorMatrix[j, 3] = 0;
-            }
-            colorMatrix[3, 0] = 0;
-            colorMatrix[3, 1] = 0;
-            colorMatrix[3, 2] = 0;
-            colorMatrix[3, 3] = 0.5;
-          } else {
-            colorMatrix[0, 4] = 0.5;
-            colorMatrix[3, 3] = 1;
-          }
-        }
-        device.update(drawableCollection);
-      }
+  public static void update(Device, Drawable)(Device device, Drawable drawable) {
+    MSG msg;
+    if (PeekMessage(&msg, null, 0, 0, PM_REMOVE)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    } else {
+      Sleep(1);
+      device.update(drawable);
     }
-    return cast(int)msg.wParam;
+    if (msg.message == WM_QUIT) {
+      throw new ExitException(cast(int)msg.wParam);
+    }
   }
 
 }
--- src/main.d
+++ src/main.d
@@ -2,6 +2,7 @@ module main;
 
 import kaiko.game.application;
 import kaiko.game.device;
+import kaiko.game.mainscene;
 import kaiko.game.mainwindow;
 import kaiko.ipc.simplesession;
 import kaiko.ipc.server;
@@ -18,5 +19,10 @@ import std.stdio;
 void main(string[] args) {
   auto mainWindow = new MainWindow!Application;
   auto device = new Device(mainWindow.handle, Application.width, Application.height);
-  return Application.run(mainWindow.handle, device);
+  auto scene = new MainScene();
+  try {
+    return Application.run(mainWindow.handle, device, scene);
+  } catch (ExitException e) {
+    return e.status;
+  }
 }
